f9ea27bbdd098c25ca11313bc921c9278bd5b4cf
Sandra <sandra.frontdev@gmail.com>
2025-03-26 16:39:32 +0100

    fixing footer color

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 export const Footer: FC = () => {
   return (
-    <footer className="bg-gray-100 py-4 mt-auto">
+    <footer className="bg-gray-100 py-4 mt-10">
       <p className="text-center text-gray-600 text-sm">
         Created by <strong>Sandra Mitić</strong> | Test task
       </p>
